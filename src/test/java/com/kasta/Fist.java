186d83e7ee239ac9e26a9d16ae40a6ec82fbd5f1
Ira <irinakyzomkina@gmail.com>
2022-01-11 10:42:26 +0300

    Two commit

--- src/test/java/com/kasta/Fist.java
+++ src/test/java/com/kasta/Fist.java
@@ -2,7 +2,9 @@ package com.kasta;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Fist {
@@ -19,6 +21,13 @@ public class Fist {
     @Test
     public void findSite() {
         driver.get("https://kasta.ua/uk/");
+    }
+
+    @Test
+    public void findButton() {
+        driver.get("https://kasta.ua/uk/");
+        WebElement element= driver.findElement(By.xpath(".//label[@for='2dfaa991-2238-f03a-93c5-08da3d3ebdad']"));
+        element.click();
     }
 
 
